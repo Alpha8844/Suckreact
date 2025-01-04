ff60f49f5d055db1383bbd4247fd2b433782e715
Caelus Rex <94648887+Alpha8844@users.noreply.github.com>
2025-01-04 22:24:45 +0000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
   },
 }) 
