1f03e2c7628af420a6f8e7748afa72c74f5ccb19
Caelus Rex <94648887+Alpha8844@users.noreply.github.com>
2025-01-05 17:06:43 +0000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,15 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  }
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 }) 
